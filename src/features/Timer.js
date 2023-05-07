a928ac2e2c2ef1b7e1b9fbb6145c92c812bbc4c0
Marcio Curvello <marciocurvello@gmail.com>
2023-05-07 12:54:50 -0300

    add Reset timer

--- src/features/Timer.js
+++ src/features/Timer.js
@@ -23,6 +23,13 @@ export const Timer = ({ focusSubject, clearSubject }) => {
   const [progress, setProgress] = useState(1);
   const [minutes, setMinutes] = useState(0.1);
 
+  const onEnd = (reset) => {
+    Vibration.vibrate(PATTERN);
+    setIsStarted(false);
+    setProgress(1);
+    reset();
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.countdown}>
@@ -30,9 +37,7 @@ export const Timer = ({ focusSubject, clearSubject }) => {
           minutes={minutes}
           isPaused={!isStarted}
           onProgress={setProgress}
-          onEnd={() => {
-            Vibration.vibrate(PATTERN);
-          }}
+          onEnd={onEnd}
         />
         <View style={styles.content}>
           <Text style={styles.title}>Focusing on:</Text>
