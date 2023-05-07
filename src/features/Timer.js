791f0da0160bcd49c5bcd0261d4366d9fc585c69
Marcio Curvello <marciocurvello@gmail.com>
2023-05-07 11:44:58 -0300

    add button to start or pause the timer

--- src/features/Timer.js
+++ src/features/Timer.js
@@ -1,12 +1,27 @@
 import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Countdown } from "../components/Countdown";
+import { RoundedButton } from "../components/RoundedButton";
 
 export const Timer = ({ focusSubject }) => {
+  const [isStarted, setIsStarted] = useState(false);
+
   return (
     <View style={styles.container}>
       <View style={styles.countdown}>
-        <Countdown isPaused onProgress={() => {}} onEnd={() => {}} />
+        <Countdown
+          isPaused={!isStarted}
+          onProgress={() => {}}
+          onEnd={() => {}}
+        />
+      </View>
+      <View style={styles.buttonWrapper}>
+        {!isStarted && (
+          <RoundedButton title="start" onPress={() => setIsStarted(true)} />
+        )}
+        {isStarted && (
+          <RoundedButton title="pause" onPress={() => setIsStarted(false)} />
+        )}
       </View>
     </View>
   );
@@ -18,6 +33,14 @@ const styles = StyleSheet.create({
   },
   countdown: {
     flex: 0.5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonWrapper: {
+    flex: 0.3,
+    flexDirection: "row",
+    padding: 15,
+    justifyContent: "center",
     alignItems: "center",
   },
 });
